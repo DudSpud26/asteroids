1a42a2b56bf1c7a4bfcf1050f46c4e1db70130e1
Dudspud26 <vernonseager@gmail.com>
2025-04-05 11:32:42 -0400

    calculate dt and limit frame rate

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +17,9 @@ def main():
         
         pygame.display.flip()
 
+        # Limit the frame rate to 60 FPS
+        dt = game_clock.tick(60) / 1000.0
+       
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
