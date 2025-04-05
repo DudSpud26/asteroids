424170664fefaa37ca5887c24385ffaf706a8a0e
Dudspud26 <vernonseager@gmail.com>
2025-04-05 12:17:59 -0400

    added the player object

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+    
+    # Initialize the player
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill((0, 0, 0))
-        
+        player.draw(screen)
         
         pygame.display.flip()
 
